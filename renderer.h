b34343dde3110938d48549c143a7c2b1f34619ac
Vivien B <vivienbihl@gmail.com>
2022-07-15 13:46:12 +0200

    Use cop2 instructions for GTE macros

--- renderer.h
+++ renderer.h
@@ -14,4 +14,28 @@ void rdr_cleanup();
 unsigned int rdr_getticks();
 void rdr_delay();
 
+#undef gte_rtps
+#define gte_rtps() __asm__ volatile (\
+    "nop;"                  \
+    "nop;"                  \
+    "cop2 0x0180001;")
+
+#undef gte_rtpt
+#define gte_rtpt() __asm__ volatile (\
+    "nop;"                  \
+    "nop;"                  \
+    "cop2 0x0280030;")
+
+#undef gte_nclip
+#define gte_nclip() __asm__ volatile (\
+    "nop;"                  \
+    "nop;"                  \
+    "cop2 0x1400006;")
+
+#undef gte_avsz4
+#define gte_avsz4() __asm__ volatile (\
+    "nop;"                  \
+    "nop;"                  \
+    "cop2 0x168002E;")
+
 #endif
