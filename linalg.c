a454a571248ca7438f41e23f8771af5678ffb274
vivi168 <vivienbihl@gmail.com>
2023-12-18 12:03:29 +0900

    clang format

--- linalg.c
+++ linalg.c
@@ -62,27 +62,27 @@ void quat_rotate_point(const quat q, const vec3 in, vec3 out)
 
 // trigonometry
 
-#define qN	10
-#define qA	12
-#define B	19900
-#define	C	3516
+#define qN 10
+#define qA 12
+#define B  19900
+#define C  3516
 
 int iSin(int x)
 {
     int c, x2, y;
 
-    c= x<<(30-qN);              // Semi-circle info into carry.
-    x -= 1<<qN;                 // sine -> cosine calc
+    c = x << (30 - qN); // Semi-circle info into carry.
+    x -= 1 << qN;       // sine -> cosine calc
 
-    x= x<<(31-qN);              // Mask with PI
-    x= x>>(31-qN);              // Note: SIGNED shift! (to qN)
+    x = x << (31 - qN); // Mask with PI
+    x = x >> (31 - qN); // Note: SIGNED shift! (to qN)
 
-    x= x*x>>(2*qN-14);          // x=x^2 To Q14
+    x = x * x >> (2 * qN - 14); // x=x^2 To Q14
 
-    y= B - (x*C>>14);           // B - x^2*C
-    y= (1<<qA)-(x*y>>16);       // A - x^2*(B-x^2*C)
+    y = B - (x * C >> 14);         // B - x^2*C
+    y = (1 << qA) - (x * y >> 16); // A - x^2*(B-x^2*C)
 
-    return c>=0 ? y : -y;
+    return c >= 0 ? y : -y;
 }
 
 
